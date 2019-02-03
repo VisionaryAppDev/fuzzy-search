32f0497e575f2b000b82dc218ce4d5905a5cf4ac
Norin <>
2019-02-03 23:35:02 +0700

    init

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,30 @@
-import { window, ExtensionContext, commands, workspace, TextEditor, QuickPickItem, Position } from 'vscode';
+import { window, ExtensionContext, commands, TextEditor, QuickPickItem, Position, Selection } from 'vscode';
 
 export function activate(context: ExtensionContext) {
     context.subscriptions.push(commands.registerCommand('extension.fuzzy-search', async () => {
         let activeEditor = window.activeTextEditor;
         if (activeEditor) {
-
-
             const item: QuickPickItem[] = getTextLineByLine(activeEditor);
 
 
             // HOLD: can't put it to pallete :(
             getUserSelectedText(activeEditor);
 
-workspace.onDidChangeTextDocument
+
+
+
             const result = await window.showQuickPick(item, {
                 onDidSelectItem: item => {
+                    const selectedLine = locateSelectedLine(item.label);
+                    const position = new Position(selectedLine, 2);
+                    activeEditor.selection = new Selection(position, position);
+
+
                     window.showInformationMessage(item.label)
                 },
                 placeHolder: "place holder",
                 ignoreFocusOut: true
             });
-
-
         }
     }));
 }
@@ -35,27 +38,26 @@ export function getTextLineByLine(activeEditor: TextEditor) {
 }
 
 
-function locateSelectedLine(text: string) {
-    return text.split(/\D/)[0];
+function locateSelectedLine(text: string) : number{
+    return Number.parseInt( text.split(/\D/)[0]);
 }
 
 function getUserSelectedText(activeEditor: TextEditor) {
-    if (activeEditor.selection.active) {
-        const selectedLine = { start: activeEditor.selection.start, end: activeEditor.selection.end };
-        const isMultipleLineSelected = selectedLine.start.line !== selectedLine.end.line;
-
-
-        if (!isMultipleLineSelected) {
-            let wholeLineText = activeEditor.document.lineAt(selectedLine.start.line);
-            let selectedText = wholeLineText.text.substring(selectedLine.start.character, selectedLine.end.character);
-
-            console.log("User Selected Text is: " + selectedText);
-
-            return selectedText;
-        }
+    if (!activeEditor.selection.active){
+        return "";
     }
 
-    return "";
+    
+    const selectedLine = { start: activeEditor.selection.start, end: activeEditor.selection.end };
+    const isMultipleLineSelected = selectedLine.start.line !== selectedLine.end.line;
+    if (!isMultipleLineSelected) {
+        let currentLineText = activeEditor.document.lineAt(selectedLine.start.line);
+        let currentLineSelectedText = currentLineText.text.substring(selectedLine.start.character, selectedLine.end.character);
+
+        console.log("User Selected Text is: " + currentLineSelectedText);
+
+        return currentLineSelectedText;
+    }
 }
 
 
